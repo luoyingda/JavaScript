5d99179dbc177a00704f4acd895d4ca7d212b489
罗英达 <1374014558@qq.com>
2021-07-14 13:46:37 +0800

    update

--- JS基础知识/js/7.js
+++ JS基础知识/js/7.js
@@ -51,5 +51,35 @@ if (a > 0) {
     }
 }
 
-//=================switch case======================
-v
+//=================switch case 一个变量在不同值得情况下的不同操作======================
+// 1. 每一种CASE情况结束后最好都加上BREAK
+// 2. default等价于else，以上都不成立执行
+// 3. 每一种case情况的比较用的是===“绝对相等”
+// 不加break。当前条件成立执行完成后，后面条件不论成立都要执行，直到遇见break为止（不加break可以实现变量在不同值的情况下做相同的事情）
+let c='5';
+switch (c) {
+    case 1:
+        console.log('hehe');
+        break;
+    case 5:
+        console.log('haha');
+        break;
+    case 10:
+        console.log('heihei');
+        break;
+    default:
+        console.log('xixi');
+}
+
+if (c == 1) {
+    console.log('haha');
+} else if (c == 5) {
+    // A && B：A和B都成立才为TRUE
+    // A || B：A或B有一个成立就位TRUE
+    console.log('hehe');
+} else if (c == 10) {
+    console.log('heihei');
+} else {
+    console.log('xixi');
+}
+
