ac14ff6cfad885017c28a08ecbb7dbe2c160848a
罗英达 <1374014558@qq.com>
2021-07-16 08:31:25 +0800

    update

---
+++ JS基础知识/js/8.js
@@ -0,0 +1,37 @@
+/*
+ *  1.创建循环初始值
+ *  2.设置（验证）循环执行的条件
+ *  3.条件成立执行循环体中的内容
+ *  4.当前循环结束执行步长累计操作
+ */
+/* for (var i = 0; i < 5; i++) {
+    console.log(i);
+}
+console.log(i); */
+
+/* for (var i = 10; i > 4; i -= 2) {
+    if (i < 7) {
+        i++;
+    } else {
+        i--;
+    }
+}
+console.log(i); */
+//循环体中的两个关键词
+// continue：结束当前这轮循环（continue后面的代码不在执行），继续执行下一轮循环
+// break：强制结束整个循环（break后面的代码也不再执行），而且整个循环啥也不干直接结束
+
+//面试题
+for (var i = 0; i < 10; i++) {
+    if (i >= 2) {
+        i += 2;
+        continue;
+    }
+    if (i >= 6) {
+        i--;
+        break;
+    }
+    i++;
+    console.log(i);
+}
+console.log(i);
