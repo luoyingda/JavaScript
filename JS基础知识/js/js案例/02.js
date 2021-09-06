e9eeece1f29d4ef7a40bf1af4af7f6285da3fa64
luoyingda <1374014558@qq.com>
2021-09-06 08:06:58 +0800

    update

---
+++ JS基础知识/js/js案例/02.js
@@ -0,0 +1,6 @@
+//实现queryURLParameter
+let url = "http://www.zhufengpeixun.cn/index.html?lx=1&name=zhufeng&teacher=aaa#box";
+
+//获取问号后面的值
+let askIndex=url.indexOf('?');
+let sharpIndex=url.indexOf('#');
